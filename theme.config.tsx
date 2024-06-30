ce77f9c4436c34ff4d147141c1ab8118e79e4a3a
Charles2029 <love98982016@gmail.com>
2024-07-01 07:48:11 +0800

    second push

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Charles Blog</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Charles2029',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://github.com/Charles2029',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Charles2029',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Welcome to Charles space',
   },
 }
 
